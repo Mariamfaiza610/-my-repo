76583327a7f4c817612331b5241b02b39ccd7a2a
Mariamfaiza610 <mariamfaizan22@gmail.com>
2024-12-29 20:55:31 +0500

    Add files via upload

---
+++ coding.py
@@ -0,0 +1,158 @@
+name : str ="mariam"
+age  : str ="25"
+is_active: bool = True
+person= name + age
+print (person)
+
+
+
+
+#arithemetic opearator
+#assingment opearator
+#comparision opearator  
+#logical opearator
+
+
+
+
+
+#arithematic opearator   (+,-,*,?)
+num1 : int=5
+num2 : int=10
+
+#addition (+)
+sum : int = num1 + num2
+print (sum)
+
+#subtraction (-)
+sub = num1-num2
+print (sub)
+
+#multiply (*)
+mul = num2*num1
+print(mul)
+
+#division (/)
+div = num1/num2
+print (div)
+
+#modulus (%)
+num1 = 5
+num2 =10
+print (num1 % num2)
+#11/2=1
+
+#exponent (**)
+num1 =15
+num2 =5
+print (num1 ** num2)
+
+#floor division (//)
+num1 =15
+num2 =5 
+print (num1 // num2)
+
+
+
+
+#assingment opearator  (=)
+sum = num1 + num2
+sum = 5+10
+print (sum)
+
+
+sum= 15 * num1
+print (sum)
+
+
+#comparision opearator (boolean)   (==,<,>,=<,>=,!=)
+
+#equal to (==)
+num1 = 10
+num2 = 10
+print (num1==num2)
+num1=10
+num2=20
+
+#greator than opearator (>)
+num1 < num2
+print (num1<num2)
+
+#less than opearator (>)
+num1 > num2
+print(num1 > num2)
+
+#greater than or equal to (<=)
+num1 <= num2
+print (num1<=num2)
+
+#less than or equal to opearator (>=)
+num1 >= num2
+print (num1>=num2)
+
+#not equal to(!=)
+num1 != num2
+print (num1!=num2)
+
+#logical opearator (or,not,and)
+           
+#and
+
+num1:int=10
+num2:int=11
+#condition
+#num1 is greater than 10 
+#and 
+#less than 30
+#num1 > 10 and less thsn 30
+print(num1 < 10 and num1 > 30)
+
+
+num1:int =20
+num2:int =25
+#condition
+#koi bh 1 condition true hogi to true print krrga  
+#num1 is equal to 20 
+#and 
+#num2 is not equal to 20
+#num1 =!20 and num2 >50
+print(num1 !=20 and num2 > 50)
+
+
+#or 
+#koi bhi 1 condition true  honi chahye 
+num1 =30
+num2 =20
+#condition 
+#num1 is lessthan equal to 20 
+#OR 
+#NUM2 IS GREATER THAN NOT EQUAL TO NUM 1
+#NUM1 >= 20 OR NUM2 <= NUM1 
+print(num1 <=20 or num2 >= num2)
+
+#not
+#kisi bhi true condition ko false krna hoto not use krege 
+#or
+#kisi bhi false condition ko true krna hoto not use krege 
+
+#condition 
+num1 = 25
+num2= 35
+prinq(num1 == num2 or num1 > num2)
+print(not num1== num2)
+print(not num1 > num2)
+
+
+#(if)ager    (else)warna 
+if 20 != 30 or 30 >31:
+    print ("mariam faizan")
+
+
+if 20==20 or 20==10 :
+    print("amna")   
+
+
+amna = 10 
+mariam = 20 
+if  amna != mariam 
+    print("ibrahim")
